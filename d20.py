973e8140e202ee828d3c65b024e36ec984f1d8fa
Josiah Kiok <josiahkiok@Josiahs-MacBook-Pro.local>
2022-12-28 14:26:44 -0600

    d20 which took me 1.5 hours longer than it should have

--- d20.py
+++ d20.py
@@ -1,17 +1,25 @@
-d = {}
-
-class Number():
-    def __init__(self, index, value):
-        self.index = index
+class Node:
+    def __init__(self, value, activation):
         self.value = value
+        self.activation = activation
 
 with open("inp20.txt") as f:
     c = [int(i.strip()) for i in f.readlines()]
 
-#d = {index:i for index, i in enumerate(c)} 
-d = {index:Number(index, i) for index, i in enumerate(c)} # dictionary used just to find ordering i suppose
-l = [d[i] for i in range(len(c))]
+circ = []
+for i in range(len(c)):
+    circ.append(Node(c[i]*811589153, i))
 
-for i in range(5):
-    # we want to shift d[i] 
-    l.insert((d[i].index)%5, l.pop(d[i]))
+for round in range(10):
+    print (round)
+    for i in range(len(c)):
+        for j in range(len(c)):
+            if circ[j].activation == i:
+                temp = circ[j]
+                circ.pop(j)
+                circ.insert((temp.value+j) % (len(c)-1), temp)
+                break
+
+for i in range(len(c)):
+    if circ[i].value == 0:
+        print (circ[(i+1000)%len(c)].value+circ[(i+2000)%len(c)].value+circ[(i+3000)%len(c)].value)
